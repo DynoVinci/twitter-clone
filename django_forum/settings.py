f133f9c04cc03f2909693b5982a5db59d45ebbf2
Darris Lee George Jr <Vinci@Darriss-MacBook-Pro.local>
2021-12-05 21:34:52 -0500

    updated

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -135,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
@@ -152,3 +153,4 @@ cloudinary.config(
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
